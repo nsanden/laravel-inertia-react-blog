f2b319f29560e3fc7971d2aeaaf234bea81ab77d
Nate Sanden <natesanden@gmail.com>
2025-06-16 21:06:21 -0400

    Include FontAwesome assets in main publish tag for simpler installation

--- src/BlogServiceProvider.php
+++ src/BlogServiceProvider.php
@@ -21,17 +21,13 @@ class BlogServiceProvider extends ServiceProvider
             __DIR__.'/../resources/js/Pages' => resource_path('js/Pages/Blog'),
             __DIR__.'/../resources/js/Components' => resource_path('js/Components/Blog'),
             __DIR__.'/../resources/js/Layouts' => resource_path('js/Layouts/Blog'),
+            __DIR__.'/../node_modules/@fortawesome/fontawesome-free/css/all.min.css' => public_path('css/fontawesome.css'),
+            __DIR__.'/../node_modules/@fortawesome/fontawesome-free/webfonts' => public_path('webfonts'),
         ], 'laravel-inertia-react-blog');
 
         // Publish the routes file
         $this->publishes([
             __DIR__.'/../routes/web.php' => base_path('routes/blog.php'),
         ], 'laravel-inertia-react-blog-routes');
-
-        // Publish FontAwesome CSS and fonts
-        $this->publishes([
-            __DIR__.'/../node_modules/@fortawesome/fontawesome-free/css/all.min.css' => public_path('css/fontawesome.css'),
-            __DIR__.'/../node_modules/@fortawesome/fontawesome-free/webfonts' => public_path('webfonts'),
-        ], 'laravel-inertia-react-blog-fontawesome');
     }
 }
